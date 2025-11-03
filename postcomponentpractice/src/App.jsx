af26ab5afb17a5f5631cefc8b552987f83e50679
bhavesh joshi <joshbhavesh10@gmail.com>
2025-11-03 18:35:11 +0530

    done all of that

--- postcomponentpractice/src/App.jsx
+++ postcomponentpractice/src/App.jsx
@@ -2,43 +2,17 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-
-  return <>
-      <div>
-        <div style={{display : "flex" , justifyContent : "center", marginTop:"1rem"}}>
-            <Postcomponent name = "bhavesh joshi" Followers="123 Followers"  Time = "3m Ago"></Postcomponent>
-        </div>
-        <div style={{display : "flex" , justifyContent : "center", marginTop:"1rem"}}>
-            <Postcomponent name = "Guarav Sati" Followers="321 Followers"  Time = "56m Ago"></Postcomponent>
-        </div>
-        <div style={{display : "flex" , justifyContent : "center", marginTop:"1rem"}}>
-            <Postcomponent name = "bhashkar fhulara" Followers="3456 Followers"  Time = "1m Ago"></Postcomponent>
-        </div>
-      </div>
+  return<>
+    {[  <Card name={"Guarav"} time={"0:56am"} ></Card> 
+      , <Card name={"bhavesh"} time={"8:56am"} ></Card> 
+    ]}
   </>
-    
 }
-function Postcomponent({ name , Followers , Time})
+function Card(props)
 {
-  const [Visible , SetVisible] = useState(true);
-  
-  function changevisibility()
-  {
-    SetVisible(!Visible);
-  }
-  return <>
-    <button onClick={changevisibility} style={{border:"none" , marginRight : "2rem" , borderRadius : "1rem" , cursor:"pointer"}}>Doit</button>
-    <div style={{padding: "1rem" , borderRadius:"2rem" , backgroundColor:"#bdc3c7" , width:"20rem"}}>
-        <div style={{display:"flex"}}>
-          <div><img src={"../randompic.jpg"} alt="" style={{width:"4rem" , borderRadius:"2rem" , marginRight:"2rem"}} /></div>
-          <div>
-            <div style={{fontSize:"1.2rem" , fontWeight:"900"}}>{name}</div>
-            <div style={{fontWeight:"700"}}>{Followers}</div>
-            {Visible && <div>{Time}</div>}
-          </div>
-        </div>
-        <div style={{fontSize:"1.2rem" , fontWeight:"900" , marginTop:"1rem" , marginBottom:"1rem" , marginLeft:"2rem"}}>Want to Know how to win big? Check out how these these folks got a $6000 bounties ! </div>
-    </div>
+  return<>
+    <h1>Name is {props.name}</h1>
+    <h1>Time is {props.time}</h1>
   </>
 }
 
--- props/vite.config.js
+++ props/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    hot: true, // ensures HMR is enabled
+    watch: {
+      usePolling: true, // helpful in WSL, Docker, or networked filesystems
+    },
+  },
 })
