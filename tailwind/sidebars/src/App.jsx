08dd154ba81e5d6f532ccb96f8accdc66751db06
bhavesh joshi <joshbhavesh10@gmail.com>
2025-11-13 04:42:18 +0530

    Designed a side bar using tailwind

---
+++ tailwind/sidebars/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  
+
+  return <>
+  <div className='flex h-screen'>
+      <div className='transition-all duration-1000 bg-red-700 -translate-x-96 w-0 md:translate-x-0 md:w-89' >
+      </div>
+      <div className='bg-green-500 w-screen'>Content</div>
+  </div>
+  </>
+}
+
+export default App
