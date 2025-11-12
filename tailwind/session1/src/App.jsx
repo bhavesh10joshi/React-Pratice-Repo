02e548a6794d89c27b261f565bb51ae5eeb91a7f
bhavesh joshi <joshbhavesh10@gmail.com>
2025-11-12 19:37:16 +0530

    Made an otp entering page in react with dynamic otp entering logic and backspace sequence , but the backkspace sequence is not working for this one . Has to work on this one more !

--- tailwind/session1/src/App.jsx
+++ tailwind/session1/src/App.jsx
@@ -1,24 +1,97 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const ref1 = useRef();
+  const ref2 = useRef();
+  const ref3 = useRef();
+  const ref4 = useRef();
+  const ref5 = useRef();
   return (
     <>
-      <div className='bg-sky-950 h-185.5'>
-        <div className='flex justify-center h-30 pt-10'>
-          <div className='pt-2'> <svg width="30px" height="30px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="white"><path d="M7 21L17 21" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M13.5 7L13.5 11M13.5 13L13.5 11M13.5 11L14.8706 9.43363M17 7L14.8706 9.43363M14.8706 9.43363L17 13" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M9.5 7L6.5 11.5L10 11.5L10 13" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M2 16.4V3.6C2 3.26863 2.26863 3 2.6 3H21.4C21.7314 3 22 3.26863 22 3.6V16.4C22 16.7314 21.7314 17 21.4 17H2.6C2.26863 17 2 16.7314 2 16.4Z" stroke="#ffffff" stroke-width="1.5"></path></svg> </div>
-          <div class='text-[#00b8db] ml-2 text-[30px]'>Webinar</div>
-          <div class='text-white text-[30px]'>.gg</div>
-        </div>
-        <div className='text-white text-[30px] flex justify-center h-30 pt-10 font-bold pb-2'> Verify Your Age</div>
-        <div className='text-[#74d4ff] flex justify-center pb-10'> Please confirm your birth year . This data will not be stored . </div>
-        <div className='flex justify-center pb-10'><input type="text" placeholder='Your Birth Year' className='bg-[#00598a] h-12 w-70 flex justify-center rounded-lg pl-2'/></div>
-        <div className='flex justify-center'> <button className='bg-[#00a6f4] h-10 w-70 flex justify-center items-center rounded-lg '>Continue</button ></div>
+      <div className='bg-sky-950 h-185.5 flex justify-center'>
+        <Otp reference={ref1} onDone={function()
+          {
+            ref2.current.focus();
+          }
+        }NotDone={function()
+            {
+              ref1.current.focus();
+            }
+          }></Otp>
+        <Otp reference={ref2} onDone={function()
+          {
+            ref3.current.focus();
+          }
+        }NotDone={function()
+            {
+              ref1.current.focus();
+            }
+          }/>
+        <Otp reference={ref3} onDone={function()
+          {
+            ref4.current.focus();
+          }
+        }NotDone={function()
+            {
+              ref2.current.focus();
+            }
+          }/>
+        <Otp reference={ref4} onDone={function()
+          {
+            ref5.current.focus();
+          }
+        }
+        NotDone={function()
+            {
+              ref3.current.focus();
+            }
+          }/>
+        <Otp reference={ref5} onDone={function()
+          {
+            ref5.current.focus();
+          }
+        }NotDone={function()
+            {
+              ref4.current.focus();
+            }
+          }/>
+
       </div>
     </>
   )
 }
 
+function Otp({reference , onDone , NotDone})
+{
+  const[curr , setcurrvalue] = useState("");
+  
+  return<>
+    <input type="text" value={curr} ref={reference} className='bg-[#74d4ff] h-14 w-12 rounded-lg ml-2 mt-10 text-black-500 p-5'onChange={function(e)
+      {
+        const val = e.target.value;
+        const valnum = Number(val);
+        if(!isNaN(valnum) && val!="") 
+        {   
+          console.log(e.key);
+          setcurrvalue(val);
+          e.target.value = curr; 
+          onDone();
+        }
+      }
+      }
+      onKeyDown={function(e)
+      {
+        console.log("hi");
+        if(e.key == " Backspace")
+        {
+          setcurrvalue("");
+          e.target.value = curr; 
+          NotDone();
+          console.log(e.key);
+        }
+      }
+    }/>
+  </>
+}
 export default App
